ff1a4a3d065d1537dcb0b8c2824ef2504290ff61
Jeseoyun <jeeeseo98@gmail.com>
2024-08-07 00:03:56 +0900

    등산중2

--- Je/8_2w/0805_등산로.py
+++ Je/8_2w/0805_등산로.py
@@ -39,9 +39,11 @@ def hiking_trail_bfs(hiking_map, N, K, start_x, start_y):
     # 시작 지점 초기값
     queue = deque([(start_x, start_y, False, 1)])
     visited[start_x][start_y] = True
+    max_length = 1
         
     while queue:
         x, y, FLAG, length = queue.popleft()
+        max_length = max(max_length, length)
         
         for dx, dy in dxy:
             nx, ny = x + dx, y + dy
@@ -57,16 +59,21 @@ def hiking_trail_bfs(hiking_map, N, K, start_x, start_y):
             # 1. 이동할 지점이 더 낮은 경우 -> 그 지점으로 바로 이동
             if hiking_map[nx][ny] < hiking_map[x][y]:
                 visited[nx][ny] = True
-                queue.append((nx, ny, ))
+                queue.append((nx, ny, False, length+1))
             
             # 2. 이동할 지점이 더 높거나 같은 경우(elif 조건문 사용)
             # K만큼 깎을 수 잇는 기회 1번 있음 
             # K를 썼는지 안썼는지 알기 위해 FLAG를 두자
             # -> 이전에 K만큼 지형 깎은 적 없으면 깎고 이동
-            elif not FLAG and hiking_map[x][y] - hiking_map[nx][ny] < K:
-                visited[nx][ny] = True
-                queue
-                # 아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+            # 2. 이동할 지점이 더 높거나 같은 경우
+            elif not FLAG:
+                # 최소로 깎아 이동할 수 있는 경우
+                if hiking_map[nx][ny] - hiking_map[x][y] + 1 <= K:
+                    visited[nx][ny] = True
+                    hiking_map[nx][ny] = hiking_map[x][y] - 1
+                    queue.append((nx, ny, True, length+1))
+
+    return max_length
 
 
 def main():
@@ -78,7 +85,6 @@ def main():
         
         # 1. 가장 높은 지점 찾기
         start_points = find_start_point(hiking_map, N)
-        print(start_points)
         
         # 2. bfs 순회
         root_length = []
@@ -86,8 +92,8 @@ def main():
             root_length.append(hiking_trail_bfs(hiking_map, N, K, start_x, start_y))
         
         result = max(root_length)
-        print(f"#{test_case}")
-        break
+        print(f"#{test_case} {result}")
+        # 하 답이 몇 개는 맞고 몇 개는 틀림
 
 
 if __name__ == "__main__":
