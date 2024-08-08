8729c1894f70bc7fa1734c1ab10696a7b0e5c5fc
Jeseoyun <jeeeseo98@gmail.com>
2024-08-08 16:30:15 +0900

    꿀벌꿀

--- Je/8_2w/0808_벌꿀채취.py
+++ Je/8_2w/0808_벌꿀채취.py
@@ -20,8 +20,6 @@ def honey_comb(arr, max_honey, idx, comb, combs):
         combs.append(comb[:])
         return comb[:]
     
-
-    
     # 포함 하는 경우
     honey_comb(arr, max_honey, idx+1, comb + [arr[idx]], combs)
     # 포함 안하는 경우
@@ -51,30 +49,34 @@ def main():
         for a_i in range(N):
             for a_j in range(N - M + 1):
                 worker_A = beehive[a_i][a_j:a_j+M]
+                
+                # 2-1. A 꿀통 채취
+                # 영역 내에서 꿀을 채취할 수 있는 모든 경우의 수를 구하고
+                # 꿀 양의 합이 C를 넘지 않는 시점에서 최댓값
+                a_combs = []
+                honey_comb(worker_A, C, 0, [], a_combs)
+                a_profit = cal_max_profit(a_combs)
 
-            for b_i in range(a_i, N):
-                for b_j in range(N-M+1):
-                    # A와 B가 같은 줄일 때 A랑 B랑 일이 겹치면 안됨
-                    # -> B의 시작 지점이 A가 끝난 이후가 될 수 있도록 함
-                    if a_i == b_i and a_j+M > b_j:
-                        continue
+                for b_i in range(a_i, N):
+                    for b_j in range(N-M+1):
+                        # A와 B가 같은 줄일 때 A랑 B랑 일이 겹치면 안됨
+                        # -> B의 시작 지점이 A가 끝난 이후가 될 수 있도록 함
+                        if a_i == b_i and a_j+M > b_j:
+                            continue
 
-                    worker_B = beehive[b_i][b_j:b_j+M]
+                        worker_B = beehive[b_i][b_j:b_j+M]
 
-                    # 2. 꿀을 채취
-                    # 영역 내에서 꿀을 채취할 수 있는 모든 경우의 수를 구하고
-                    # 꿀 양의 합이 C를 넘지 않는 시점에서 최댓값
-                    a_combs, b_combs = [], []
+                        # 2-2. B 꿀통 채취
+                        b_combs = []
+                        honey_comb(worker_B, C, 0, [], b_combs)
 
-                    honey_comb(worker_A, C, 0, [], a_combs)
-                    honey_comb(worker_B, C, 0, [], b_combs)
+                        # print("꿀통 리밋:",C)
+                        # print(a_combs, b_combs)
 
-                    # print(a_combs, b_combs)
-
-                    # 3. 수익 계산 -> 최댓값 찾기
-                    profit = cal_max_profit(a_combs) + cal_max_profit(b_combs)
-                    max_profit = max(profit, max_profit)
-        
+                        # 3. 수익 계산 -> 최댓값 찾기
+                        b_profit = cal_max_profit(b_combs)
+                        max_profit = max(a_profit + b_profit, max_profit)
+            
         print(f"#{test_case} {max_profit}")
 
 
