2a505c3a441763806e8e93f460daf774006ca8f9
ijh0614 <ijh0614@naver.com>
2024-10-16 17:56:02 +0900

    completed

--- Hoon/1014_BOJ_숫자고르기.py
+++ Hoon/1014_BOJ_숫자고르기.py
@@ -18,15 +18,13 @@ def dfs(num_arr, index_set, num_set, cur_num, max_num):
         return
 
     if len(index_set) + max_num - cur_num <= max_size:
+        print(index_set, len(index_set) + max_num - cur_num, max_size)
         return
 
-    #선택을 안하려면 num_set에 현재 index가 없어야 함.
-    if num_arr[cur_num] not in num_set:
-        dfs(num_arr, index_set, num_set, cur_num+1, max_num)
 
     if num_arr[cur_num] in num_set:
+        # print("중복 선택 가지치기:", num_set, num_arr[cur_num])
         return
-
     #숫자를 넣어야 함. index 아님.
     #숫자를 넣으려면 현재 인덱스가 넘지 않았거나, index_set에 있으면 됨.
     if cur_num <= num_arr[cur_num] or num_arr[cur_num] in index_set:
@@ -35,6 +33,13 @@ def dfs(num_arr, index_set, num_set, cur_num, max_num):
         dfs(num_arr, index_set, num_set, cur_num+1, max_num)
         index_set.remove(cur_num+1)
         num_set.remove(num_arr[cur_num])
+
+    #선택을 안하려면 num_set에 현재 index가 없어야 함.
+    if num_arr[cur_num] not in num_set:
+        # print("선택 안하는 DFS로:",num_set, num_arr[cur_num])
+        dfs(num_arr, index_set, num_set, cur_num+1, max_num)
+    # else:
+        # print("선택 안하는 DFS 가지치기:",num_set, num_arr[cur_num])
     
     return
 
