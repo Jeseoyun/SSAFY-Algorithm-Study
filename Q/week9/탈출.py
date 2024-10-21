959feaaefe1f4fec4b055d4bc5a9e9c398e761f9
Daekyue <howeorb@naver.com>
2024-10-21 16:20:03 +0900

    merge commit}

--- Q/week9/탈출.py
+++ Q/week9/탈출.py
@@ -18,13 +18,13 @@ exit_x, exit_y = -1, -1  # 비버 굴 위치
 # 초기 상태 설정
 for i in range(r):
     for j in range(c):
-        if arr[i][j] == '*':  
+        if arr[i][j] == '*':  # 물이 있는 곳
             w_q.append((i, j))
-            visit_water[i][j] = 0 
-        elif arr[i][j] == 'S':
+            visit_water[i][j] = 0  # 물의 초기 위치
+        elif arr[i][j] == 'S':  # 고슴도치 시작 위치
             ramge_q.append((i, j))
             visit_ramge[i][j] = 0
-        elif arr[i][j] == 'D':
+        elif arr[i][j] == 'D':  # 비버의 굴 위치
             exit_x, exit_y = i, j
 
 # 물 BFS
@@ -34,7 +34,7 @@ while w_q:
         nx = cur_x + dx[i]
         ny = cur_y + dy[i]
         if 0 <= nx < r and 0 <= ny < c and visit_water[nx][ny] == -1:
-            if arr[nx][ny] == '.':
+            if arr[nx][ny] == '.':  # 빈 공간으로만 물이 확장됨
                 w_q.append((nx, ny))
                 visit_water[nx][ny] = visit_water[cur_x][cur_y] + 1
 
